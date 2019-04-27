de1bf867f1e0566c2259fb0bba99f2d567e5d64c
Andrey Kuzmin <andrey.kuzmin@soundcloud.com>
2019-04-27 11:59:57 +0200

    Support default grouping

--- ui/app/src/Main.elm
+++ ui/app/src/Main.elm
@@ -73,7 +73,7 @@ init flags url key =
                 Api.makeApiUrl url.path
 
             else
-                Api.makeApiUrl "http://localhost:9093/"
+                Api.makeApiUrl "http://http.alertmanager.staging.prometheus.srv.dt.s-cloud.net:9093"
 
         libUrl =
             if prod then
